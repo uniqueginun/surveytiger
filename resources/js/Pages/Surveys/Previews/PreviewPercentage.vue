08ec6c1bd1674dca06beb8350362ad94099526a5
osman mohammed <uniqueginun@gmail.com>
2021-11-05 21:23:42 +0300

    change the styling of progress bar

--- resources/js/Pages/Surveys/Previews/PreviewPercentage.vue
+++ resources/js/Pages/Surveys/Previews/PreviewPercentage.vue
@@ -1,40 +1,43 @@
 <template>
-    <preview-card :questiontext="question.question_text">
-        <div v-for="(answer, index) of answers" :key="index" class="progress mb-2" style="height: 2rem;">
-            <div
-                aria-valuemax="100"
-                aria-valuemin="0"
-                :aria-valuenow="answer.percentage"
-                class="progress-bar"
-                role="progressbar"
-                :style="`width: ${answer.percentage}%`">
-                {{ answer.answer_text }} ({{ answer.percentage }}%)
-            </div>
-        </div>
-    </preview-card>
+  <preview-card :questiontext="question.question_text">
+    <div v-for="(answer, index) of answers" :key="index" class="mb-2">
+      <div class="d-flex align-items-center justify-content-between mb-1">
+        <span>{{ answer.answer_text }}</span>
+        <span class="text-muted">{{ answer.percentage }}%</span>
+      </div>
+      <div class="progress mb-2" style="height: 10px">
+        <div
+          aria-valuemax="100"
+          aria-valuemin="0"
+          :aria-valuenow="answer.percentage"
+          class="progress-bar"
+          role="progressbar"
+          :style="`width: ${answer.percentage}%`"
+        ></div>
+      </div>
+    </div>
+  </preview-card>
 </template>
 
 <script>
-
 import PreviewCard from "./PreviewCard";
-import {percentageMixin} from "../../../Utils/minxin";
+import { percentageMixin } from "../../../Utils/minxin";
 
 export default {
-    name: "PreviewPercentage",
+  name: "PreviewPercentage",
 
-    components: {PreviewCard},
+  components: { PreviewCard },
 
-    props: ['question'],
+  props: ["question"],
 
-    mixins: [percentageMixin]
-}
-
+  mixins: [percentageMixin],
+};
 </script>
 
 <style scoped>
-    .progress-bar {
-        background-color: #38c172;
-        text-align: start;
-        padding-left: 10px;
-    }
+.progress-bar {
+  background-color: #38c172;
+  text-align: start;
+  padding-left: 10px;
+}
 </style>
